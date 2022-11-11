9b2ea85675c9a85eb4cd5faf354181795713d0b4
felipe.ospina <felipe.ospina@globant.com>
2022-11-11 11:48:30 -0500

    add fade in animation to text cards

--- src/features/Game/components/ScorePoints.tsx
+++ src/features/Game/components/ScorePoints.tsx
@@ -1,33 +1,47 @@
-import { Pressable, View } from 'react-native';
+import { Point, useStore } from '@zustandStore';
+import { useRef } from 'react';
+import { Animated, Pressable, View } from 'react-native';
 import { InfoText } from './InfoText';
 import { Target } from './Target';
-import { Point, useStore } from '@zustandStore'
 
 interface TargetProps {
   onPress: () => void;
   point: Point;
 }
 
-export function ScorePoints({ onPress, point:{ text, coordinates,id, isPressed, showText}}: TargetProps) {
-  const { markAsPressed, toggleText } = useStore()
+export function ScorePoints({
+  onPress,
+  point: { text, coordinates, id, isPressed, showText },
+}: TargetProps) {
+  const { markAsPressed, toggleText } = useStore();
+  const fadeAnimation = useRef(new Animated.Value(0)).current;
+
+  function fadeIn() {
+    Animated.timing(fadeAnimation, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }
 
   function handlePress() {
     markAsPressed(id);
-    toggleText(id)
+    toggleText(id);
+    if (!showText) fadeIn();
     if (!isPressed) onPress();
   }
   return (
     <View className="absolute top-10 left-10 flex items-center w-1/4" style={coordinates}>
-      <View className="w-full h-20">
+      <Animated.View className="w-full h-20" style={{ opacity: fadeAnimation }}>
         <Pressable
           accessible
           accessibilityLabel="toca para cerrar"
           accessibilityHint="cierra el modal con texto"
-          onPress={()=>toggleText(id)}
+          onPress={() => toggleText(id)}
         >
           {showText && <InfoText>{text}</InfoText>}
         </Pressable>
-      </View>
+      </Animated.View>
       <Target
         isPressed={isPressed}
         onPress={handlePress}
